97c0b3d9ebf9d4cf5e0ae53bc027ac45eca0f997
MrBusiness87 <bbartula87@gmail.com>
2020-08-10 22:50:45 -0400

    08/10/2020-Creating File Structure for application to deploy later.

---
+++ db/burgers.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR
+    (50) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+    );
+
+    INSERT INTO burgers
+        (burger_name, devoured)
+    VALUES
+        ("hamburger", false),
+        ("cheeseburger", false),
+        ("double cheese burger", false);
+
+    SELECT *
+    FROM burgers;
